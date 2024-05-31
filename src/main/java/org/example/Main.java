cafe9f57b940a21155f411472c2ec14fbf765477
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2024-05-31 16:03:24 +0200

    Added touch-ups

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,20 @@
 package org.example;
 
+/*
+    Website Monitor
+ */
+
 public class Main {
     public static void main(String[] args) {
+        // Initialise user
         User user = new User();
+
+        // Subscribe to websites
+        // TODO: Implement GUI for user input
         user.subscribe("https://gist.githubusercontent.com/astralvoid-dev/e5a383a13138a836194ab53307266cdd/raw", 5, Channel.WEB);
         user.subscribe("https://teaching.dahahm.de", 10, Channel.EMAIL);
 
+        // Check for updates
         user.getSubscriptionSystem().checkForUpdates();
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -6,18 +6,25 @@ package org.example;
  */
 
 public class User {
+    // Each user has their own subscription system
    private final SubscriptionSystem subscriptionSystem;
 
    public User() {
        this.subscriptionSystem = new SubscriptionSystem();
    }
    public void subscribe(String link, int frequency, Channel channel) {
+       // Resolve link to website
        Website subscribedWebsite = new Website(link);
+
+       // Resolve notification frequency and channel for website
        subscribedWebsite.setNotification(new Notification(frequency, channel));
+
+       // Register to subscription list
        System.out.println("Subscribing to " + link + "...");
        subscriptionSystem.registerWebsite(subscribedWebsite);
    }
    public void unsubscribe(String link) {
+       // Unregister from subscription list
        System.out.println("Unsubscribing from " + link + "...");
        subscriptionSystem.unregisterWebsite(new Website(link));
    }
